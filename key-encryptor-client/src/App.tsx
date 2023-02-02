b8825ea82bb88da40a5ca35d2d7fe3bae50953a5
Matias-DR <Matias-DR@outlook.com>
2023-02-02 16:53:37 -0300

    Add - Route for sign-in component

--- key-encryptor-client/src/App.tsx
+++ key-encryptor-client/src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import KeyCreator from './modules/key-module/key-components/key-creator/key-creator';
+import KeyCreator from './modules/key/components/creator/creator';
+import SignIn from './modules/auth/components/sign-in/sign-in';
+
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/key-creator' element={<KeyCreator />}></Route>
+          <Route path='/sign-in' element={<SignIn />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
