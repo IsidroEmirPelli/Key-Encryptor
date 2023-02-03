8c0a2d8416093dd92112d0f6df07a8e25e2cf9d2
IsidroPelli <isidropelliemir@gmail.com>
2023-02-02 23:54:12 -0300

    adding changes

--- keymanager/keygenerator/models/passwords.py
+++ keymanager/keygenerator/models/passwords.py
@@ -1,10 +1,14 @@
 from django.db import models
+from ..models.users import User
 
 class Password(models.Model):
     """ Here we will create the model for the passwords """
-    email = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, null=True, blank=True)
+    password = models.CharField(max_length=255, null=True, blank=True)
+    notes = models.TextField(null=True, blank=True)
+    # only we store the user id
+    user_owner = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.name
+        return self.title
