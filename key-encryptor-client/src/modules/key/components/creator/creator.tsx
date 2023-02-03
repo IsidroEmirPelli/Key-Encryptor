b5e536dbbff9492117bc97146a248efae898dcae
Matias-DR <Matias-DR@outlook.com>
2023-02-02 21:35:50 -0300

    Add - 100% height to App

--- key-encryptor-client/src/modules/key/components/creator/creator.tsx
+++ key-encryptor-client/src/modules/key/components/creator/creator.tsx
@@ -6,11 +6,16 @@ const KeyCreator: FC = () => {
     const [title, setTitle] = useState('')
     const [userName, setUserName] = useState('')
     const [password, setPassword] = useState('')
-    const [url, setUrl] = useState('')
     const [notes, setNotes] = useState('')
 
-    const addKey = () => {
-        axios.post('<api-path>', {}).then(res => {
+    const onSubmit = () => {
+        const key = {
+            title: title,
+            userName: userName,
+            password: password,
+            notes: notes,
+        }
+        axios.post('<api-path>', key).then(res => {
             Swal.fire(res.data)
         }).then(err => {
             Swal.fire(`Error\n${err}`)
@@ -19,64 +24,71 @@ const KeyCreator: FC = () => {
     }
 
     return (
-        <div className="container text-center">
-            <div className="row">
-                <h2 className="mt-4">CREATE KEY</h2>
-            </div>
-            <div className="row">
-                <div className="col border border">
-                    <div className="mt-1">
-                        <div className="row">
-                            <div className="col">
-                                <label htmlFor="title" className="form-label">Title</label>
+        <div className="main">
+            <div className="w-100 m-auto container">
+                <form onSubmit={onSubmit}>
+                    <svg width="144" height="96" fill="currentColor" className="bi bi-key" viewBox="0 0 16 16">
+                        <path d="M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8zm4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5z" />
+                        <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+                    </svg>
+                    <h1 className="h3 mb-3 fw-normal">Create Key</h1>
+
+                    <div className="col-12 text-start">
+
+                        {/* Title */}
+                        <div className="row g-3 mt-1">
+                            <div className="input-group">
+                                <div className="col-2 text-start">
+                                    <label htmlFor="title" className="form-label">Title</label>
+                                </div>
+                                <div className="col-10 text-start">
+                                    <input type="email" className="form-control" id="title" placeholder="MyTitle" onChange={(e) => { setTitle(e.target.value) }}></input>
+                                </div>
                             </div>
-                            <div className="col">
-                                <input type="text" className="form-control" value={title} onChange={(e) => { setTitle(e.target.value) }}></input>
+                        </div>
+
+                        {/* User Name */}
+                        <div className="row g-3 mt-1">
+                            <div className="input-group">
+                                <div className="col-2 text-start">
+                                    <label htmlFor="user-name" className="form-label">User Name</label>
+                                </div>
+                                <div className="col-10 text-start">
+                                    <input type="email" className="form-control" id="user-name" placeholder="MyUserName" onChange={(e) => { setUserName(e.target.value) }}></input>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Password */}
+                        <div className="row g-3 mt-1">
+                            <div className="input-group">
+                                <div className="col-2 text-start">
+                                    <label htmlFor="password" className="form-label">Password</label>
+                                </div>
+                                <div className="col-10 text-start">
+                                    <input type="email" className="form-control" id="password" placeholder="PasswordExample" onChange={(e) => { setPassword(e.target.value) }}></input>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Notes */}
+                        <div className="row g-3 mt-1">
+                            <div className="input-group">
+                                <div className="col-2 text-start">
+                                    <label htmlFor="notes" className="form-label">Notes</label>
+                                </div>
+                                <div className="col-10 text-start">
+                                    <textarea className="form-control" id="notes" placeholder="MyKey" onChange={(e) => { setNotes(e.target.value) }}></textarea>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="mt-1">
-                        <div className="row">
-                            <div className="col">
-                                <label htmlFor="userName" className="form-label">User name</label>
-                            </div>
-                            <div className="col">
-                                <input type="text" className="form-control" value={userName} onChange={(e) => { setUserName(e.target.value) }}></input>
-                            </div>
-                        </div>
+
+                    {/* Submit Btt */}
+                    <div className="w-25 mx-auto">
+                        <button className="w-100 btn btn-lg btn-primary mt-3" type="submit">Create Key</button>
                     </div>
-                    <div className="mt-1">
-                        <div className="row">
-                            <div className="col">
-                                <label htmlFor="password" className="form-label">Password</label>
-                            </div>
-                            <div className="col">
-                                <input type="password" className="form-control" value={password} onChange={(e) => { setPassword(e.target.value) }}></input>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="mt-1">
-                        <div className="row">
-                            <div className="col">
-                                <label htmlFor="url" className="form-label">Url</label>
-                            </div>
-                            <div className="col">
-                                <input type="password" className="form-control" value={url} onChange={(e) => { setUrl(e.target.value) }}></input>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="mt-1">
-                        <div className="row">
-                            <div className="col">
-                                <label htmlFor="notes" className="form-label">Notes</label>
-                            </div>
-                            <div className="col">
-                                <input type="password" className="form-control" value={notes} onChange={(e) => { setNotes(e.target.value) }}></input>
-                            </div>
-                        </div>
-                    </div>
-                    <button onClick={addKey} className="mt-3 mb-3 btn btn-success">Save</button>
-                </div>
+                </form>
             </div>
         </div>
     )
