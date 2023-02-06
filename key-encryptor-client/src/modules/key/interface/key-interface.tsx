50a2a8b3c9e719806f785f7507e4acc5975464ce
Matias-DR <Matias-DR@outlook.com>
2023-02-06 03:00:57 -0300

    Add - basic user interface

--- key-encryptor-client/src/modules/key/interface/key-interface.tsx
+++
@@ -1,6 +0,0 @@
-export default interface KeyInterface {
-    title: string;
-    userName: string;
-    password: string;
-    notes: string;
-}
--- key-encryptor-client/src/modules/user/interface/user.tsx
+++ key-encryptor-client/src/modules/user/interface/user.tsx
@@ -1,4 +1,7 @@
+import KeyDataInterface from "src/modules/key/interface/key-data-interface";
+
 export default interface UserInterface {
     username: string;
     password: string;
+    keyList: KeyDataInterface[]
 }
