72150a8870f08bf7a2da8fff2e631f4fb629abd1
Matias-DR <Matias-DR@outlook.com>
2023-02-10 14:56:56 -0300

    Add - key creator link

--- key-encryptor-client/src/App.tsx
+++ key-encryptor-client/src/App.tsx
@@ -3,10 +3,11 @@ import { FC } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './modules/navbar/components/navbar';
 import KeyList from './modules/key/components/list/key-list';
-import KeyEditor from './modules/key/components/editor/key-editor';
+import KeyEditor from './modules/key/components/editor/editor';
 import { AuthContextProvider } from './modules/auth/context/component/auth-context';
 import SignIn from './modules/auth/components/signin/signin';
 import PrivateRoute from './modules/private-route/components/private-route';
+import KeyCreator from './modules/key/components/creator/creator';
 
 
 const App: FC = () => {
@@ -28,9 +29,9 @@ const App: FC = () => {
                         <PrivateRoute
                             renderIfSigned={true}
                             element={<Route path={'/keylist'} element={<KeyList />} />} />
-                        {/* <PrivateRoute
+                        <PrivateRoute
                             renderIfSigned={true}
-                            element={<Route path={'/keycreator'} element={<KeyEditor />} />} /> */}
+                            element={<Route path={'/keycreator'} element={<KeyCreator />} />} />
                         <PrivateRoute
                             renderIfSigned={true}
                             element={<Route path={'/keyeditor'} element={<KeyEditor />} />} />
