38f5081e459fd461e6a0ca34a8f196569e5cb48a
Matias-DR <Matias-DR@outlook.com>
2023-02-10 03:08:26 -0300

    Mod - bearer authorization header added

--- key-encryptor-client/src/modules/user/services/get-user.tsx
+++ key-encryptor-client/src/modules/user/services/get-user.tsx
@@ -4,7 +4,12 @@ import Swal from 'sweetalert2';
 
 const getUser = (): UserInterface => {
     let user = { username: '', password: '', keyList: [], img: '' }
-    axios.post('', localStorage.getItem('token'))
+    axios.post(
+        'http://127.0.0.1:8000/api/getuser',
+        {
+            headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` },
+        }
+    )
         .then(res => { user = res.data.user })
         .catch(err => { Swal.fire('Failed to loading user') })
     return user
