65575f0b313d2a612ecb40ee97a6ed751f539b39
Matias-DR <Matias-DR@outlook.com>
2023-02-10 03:11:05 -0300

    Mod - auth context counters edited

--- key-encryptor-client/src/modules/auth/context/component/auth-context.tsx
+++ key-encryptor-client/src/modules/auth/context/component/auth-context.tsx
@@ -2,6 +2,7 @@ import { createContext, FC, ReactNode, useEffect, useState } from "react";
 import UserInterface from "src/modules/user/interface/user";
 import authVerify from "../../services/auth-verify";
 import AuthContextInterface from "../interface/auth-context";
+import getUser from "src/modules/user/services/get-user";
 
 export const AuthContext = createContext<AuthContextInterface>({
     isAuth: false,
@@ -23,39 +24,23 @@ export const AuthContextProvider: FC<{ children: ReactNode }> = ({ children }) =
     const [localCounter, setLocalCounter] = useState<number>(0)
     const [tokenCounter, setTokenCounter] = useState<number>(0)
 
-
-    // useEffect(() => {
-    //     if (!isAuth) {
-    //         redirect('/login')
-    //         authVerify(setIsAuth)
-    //     }
-    //     const interval = setInterval(() => {
-    //         setCounter(counter + 1)
-    //     }, 20000);
-    //     return () => clearInterval(interval)
-    // }, [isAuth, counter])
-
-// - Eixsten 2 contadores:
-//     - 1 para la actividad del usuario, donde este resetea con la actividad del usuario
-//     - otro para el refresh del token, donde no resetea nunca (10 min constantes), 1 min menos del limite consulta por el token nuevo y se reinicia el contador
-
     useEffect(() => {
         const interval = setInterval(() => {
             setLocalCounter(localCounter + 1)
-        }, 72000);
+        }, 1200000);
         return () => clearInterval(interval)
     }, [isAuth, localCounter])
 
-
-    // Contador TOKEN
+    // Contador fijo para REFRESH
     useEffect(() => {
-        const interval = setInterval(() => {
-            setTokenCounter(tokenCounter + 1)
-        }, 68400);
-        authVerify(setIsAuth)
-        return () => clearInterval(interval)
-    }, [isAuth, tokenCounter])
-
+        let intervalId: any
+        if (isAuth) {
+            intervalId = setInterval(() => {
+                authVerify(setIsAuth)
+            }, 1140000)
+        }
+        return () => { clearInterval(intervalId) }
+    }, [isAuth])
 
     return (
         <AuthContext.Provider value={{ isAuth, setIsAuth, user, setUser }}>
