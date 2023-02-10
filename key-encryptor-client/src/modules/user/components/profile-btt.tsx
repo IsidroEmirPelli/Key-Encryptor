b769758f4859454237f97c1b5024115d5ad54746
Matias-DR <Matias-DR@outlook.com>
2023-02-10 14:57:10 -0300

    Add - profile link btt

--- key-encryptor-client/src/modules/user/components/profile-btt.tsx
+++ key-encryptor-client/src/modules/user/components/profile-btt.tsx
@@ -1,18 +1,31 @@
 import { FC, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import { AuthContext } from "src/modules/auth/context/component/auth-context";
+import Swal from "sweetalert2";
 
 const ProfileBtt: FC = () => {
-    const { user } = useContext(AuthContext);
+    const { user, setIsAuth } = useContext(AuthContext);
+    const nav = useNavigate();
 
     const navToProfile = () => {
-        // Acá se tiene que enviar POST con el token para recibir "permiso" (solo validar el token) de editar el perfil
-        // Si se obtiene permiso redirecciona hacia profile.tsx, sino, desloguea
+        axios.post(
+            'http://127.0.0.1:8000/api/profile',
+            user.username,
+            { headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` } }
+        )
+            .then(res => { nav('/profile') })
+            .catch(err => {
+                Swal.fire('Failed loading profile')
+                setIsAuth(false)
+            })
     }
 
     return (
         <div>
-            <img src={user.img} className="rounded mx-auto d-block" alt="Profile Pic"></img>
-            <button type="button" className="btn btn btn-link" onClick={ navToProfile }>
+            <img src="{user.img}" className="img-thumbnail" alt="profile-pic"></img>
+            {/* <img src={user.img} className="rounded mx-auto d-block" alt="Profile Pic"></img> */}
+            <button type="button" className="btn btn btn-link" onClick={navToProfile}>
                 {user.username}
             </button>
         </div>
